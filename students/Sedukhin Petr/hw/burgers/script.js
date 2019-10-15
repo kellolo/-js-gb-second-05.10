9ceb7abf35cdbf82d9fa268b8d93d89385c35468
Petr Sedukhin <petr-sed@rambler.ru>
2019-10-15 15:36:21 +0300

    fixed burgers

--- students/Sedukhin Petr/hw/burgers/script.js
+++ students/Sedukhin Petr/hw/burgers/script.js
@@ -1,58 +1,61 @@
-const size_list = {
-    'small': [50, 20],
-    'big': [100, 40]
+class Param {
+    constructor (element) {
+        this.name = element.value
+        this.price = +element.dataset ['price'] 
+        this.calories = +element.dataset ['calories']
+    }
 }
-const inner_list = {
-    'cheese': [10, 20],
-    'salad': [20, 5],
-    'potat': [15, 10]
-}
-const options_list = {
-    'spice': [15, 0],
-    'mayo': [20, 5]
-}
-let choose_size = ''
-let choose_inner = ''
-let choose_options = []
 
-class Burger{
-    constructor(){
-        this.size = 0
-        this.inner = 0
-        this.options = [0]
+class Burger {
+    constructor (size, add, toppings) {
+        this.size = new Param (this._select (size))
+        this.add = new Param (this._select (add))
+        this.toppings = this._getToppings (toppings) 
     }
 
-    calculator(ind){
-        let sum = 0
-        choose_options.forEach(el => {
-            sum += options_list[el][ind]
+    _getToppings (name) {
+        let result = []
+        this._selectAll (name).forEach (el => {
+            result.push (new Param (el))
         })
-        sum += size_list[choose_size][ind] + inner_list[choose_inner][ind]
-        return sum
+        return result
     }
 
-    selector(evt){
-        if (evt.target.classList.contains('radio-1')){
-            choose_size = el.target.dataset['id']
-        }else if(evt.target.classList.contains('radio-2')){
-            choose_inner = el.target.dataset['id']
-        }else if(evt.target.classList.contains('check')){
-            let ind = choose_options.indexOf(evt.target.dataset['id'])
-            if(ind < 0){
-                choose_options.push(evt.target.dataset['id'])
-            }else{
-               choose_options.splice(ind, 1)
-            }
-        }
+    _selectAll (name) {
+        return [...document.querySelectorAll (`input[name="${name}"]:checked`)]
+    }
+
+    _select (name) {
+        return document.querySelector (`input[name="${name}"]:checked`)
+    }
+
+    sumPrice () {
+        let result = this.size.price + this.add.price
+        this.toppings.forEach (el => {
+            result += el.price
+        })
+        return result
+    }
+
+    sumCalories () {
+        let result = this.size.calories + this.add.calories
+        this.toppings.forEach (el => {
+            result += el.calories
+        })
+        return result
+    }
+
+    showSum (price, calories) {
+        document.querySelector (price).innerText = this.sumPrice ()
+        document.querySelector (calories).innerText = this.sumCalories ()
     }
 }
 
-let burger = new Burger()
-
-document.querySelector ('.content').addEventListener ('click', burger.selector)
-
-document.querySelector('.calc-result').addEventListener ('click', function () {
-    let burger = new Burger()
-    }
-)
+window.onload = () => {	
+    document.getElementById('check').addEventListener('click', () => {
+        let burger = new Burger('size', 'add', 'topings');
+        console.log(burger);
+        burger.showSum('#price', '#calories');
+    })
+}
     
