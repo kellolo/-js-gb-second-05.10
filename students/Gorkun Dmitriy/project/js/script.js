bcbaa84591d58f1c22f29b69ecf81809533b99fd
berryllium <gorkundp@yandex.ru>
2019-10-18 16:15:13 +0300

    заменил классы и сжал картинки для корзины

--- students/Gorkun Dmitriy/project/js/script.js
+++ students/Gorkun Dmitriy/project/js/script.js
@@ -49,7 +49,7 @@ class List {
 // класс товара 
 
 class Item {
-    constructor (el, img = image) {
+    constructor (el) {
         this.product_name = el.product_name
         this.price = el.price
         this.id_product = el.id_product
@@ -73,188 +73,249 @@ class Item {
 
 //_____________________________________________________________
 
+
+// товар в каталоге
+
+class catalogItem extends Item { } 
+
+//______________________________________________________________
+
+// товар в корзине
+
+class CartItem extends Item {
+    constructor (el) {
+        super (el)
+        this.quantity = el.quantity
+        this.img2 = this.img.replace(/img/,'img/small')
+    }
+    render () {
+        return `
+            <div class="cart-item" data-id="${this.id_product}">
+                <div class="product-bio">
+                    <img src="${this.img2}" alt="Some image">
+                    <div class="product-desc">
+                        <p class="product-title">${this.product_name}</p>
+                        <p class="product-quantity">Quantity: ${this.quantity}</p>
+                        <p class="product-single-price">$${this.price} each</p>
+                    </div>
+                </div>
+                <div class="right-block">
+                    <p class="product-price">${this.quantity * this.price}</p>
+                    <button class="del-btn" data-id="${this.id_product}">&times;</button>
+                </div>
+            </div>
+        `
+    }
+}
+
+//___________________________________________________________
+
 // класс каталога
-class Catalog {
-    constructor(url) {
-        this.url = url
-        this.products = []
-        this.init()
+class Catalog extends List {
+    constructor (cart, url = CATALOG_URL, container = '.products') {
+        super (url, container)
+        this.cart = cart
+        this.getJson ()
+            .then (data => this.handleData(data))
     }
-    render() {
-        let htmlString = ''
-        this.products.forEach(function (el) { htmlString += el.render() })
-        return htmlString
-    }
-    getProduct(id) {
-       return this.products.find(item => item.id == id)
-    }
-    init() {
-        makeGETRequest(this.url)
+    _init () {
+        document.querySelector (this.container).addEventListener ('click', event => {
+            if (event.target.classList.contains('buy-btn')) {
+                this.cart.addProduct (event.target)
+            }
+        })
     }
 }
 //_____________________________________
 
-// класс продукта
-class Product {
-    constructor(id_product, product_name, price, img) {
-        this.id = id_product
-        this.name = product_name
-        this.price = price
-        this.img = img
-    }
-    render() {
-        const htmlString = `  
-                    <div class="product-item">
-                        <div class="desc">
-                        <h3 class="product-name">${this.name}</h3>
-                        <img class="img" src="${this.img}">
-                        </div>
-                        <p>Цена: <span class="product-price">${this.price}</span>$</p>
-                        <button class="buy-btn" type="button" data-id="${this.id}">В корзину</button>
-                        <div class="prod-block invisible"></div>
-                    </div>
-                    `
-        return htmlString
-    }
-}
-//_______________________________________________
-
-
-
-// HTMLHttpRequest
-const makeGETRequest = url => {
-    return new Promise((resolve, reject) => {
-    let xhr = new XMLHttpRequest()
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4) {
-            resolve(getCatalog(xhr.responseText))
-        }
-    }
-    xhr.onerror = () => reject(xhr.statusText);
-    xhr.open('GET', url, true)
-    xhr.send()
-})
-}
-//__________________________________________________________________
-
-
-
-// функция-обработчик для XHR, заполняющая каталог продуктов и выводящая его содержимое 
-function getCatalog(response) {
-    JSON.parse(response).forEach(el => {
-        catalog.products.push(new Product(el.id_product, el.product_name, el.price, el.img))
-        catalogBlock.innerHTML = catalog.render()
-    });
-}
-//__________________________________________________________________
-
-
 // класс корзины
-class Cart {
-    constructor() {
-        this.products = []
-    }
-    getSum() {
-        let sum = 0
-        this.products.forEach(el => {
-            sum += el.price * el.count
-        })
-        return sum
-    }
-    getCount() {
-        let count = 0
-        this.products.forEach(el => {
-            count += el.count
-        })
-        return count
-    }
-    getProduct(id) {
-        return this.products.find(item => item.id == id)
-    }    
-    addProduct(id) {
-        const catalogProduct = catalog.getProduct(id)
-        const cartProduct = this.getProduct(id)
-        if (catalogProduct == cartProduct) cartProduct.count++
-        else {
-            catalogProduct.count = 1
-            this.products.push(catalogProduct)
-        }
-        this.show()
-    }
-    removeProduct(id) {
-        let product = this.getProduct(id)
-        let index = this.products.findIndex(item => item.id == id);
-        if (product.count > 1) product.count--
-        else this.products.splice(index, 1)
-        if (this.products.length == 0) this.clear()
-    }
-    clear() {
-        this.products = []
-        cartBlock.innerHTML = 'Корзина пуста'
-        cartCount.innerHTML = 'Корзина пуста'
-    }
-    show() {
-        cartCount.innerHTML = `${this.getCount()} шт. на сумму ${this.getSum()}$`
-        cartCount.classList.remove('hidden')
-        setTimeout(() => cartCount.classList.add('hidden'), 2000)
-    }
 
-    render() {
-        let htmlString = ``
-        this.products.forEach(function (el) {
-            htmlString += `
-        <p>${el.name}</p>
-        <p>${el.price * el.count}$</p>
-        <p>
-            <button class="plus-btn" data-id="${el.id}">+</button>
-            <span>${el.count}</span>
-            <button class="minus-btn" data-id="${el.id}">-</button>
-        </p>
-        `
-        })
-        htmlString += `<strong>ИТОГО:</strong><strong>${this.getSum()}$</strong><p></p>`
-        htmlString += `<p></p><p></p><a href="#" class = "cart-clear">Очистить</a>`
-        cartBlock.innerHTML = htmlString
+class Cart extends List {
+    //render () => const prod = new lists [Cart] (product) // prod = new CartItem (product)
+        constructor (url = CART_URL, container = '.cart-block') {
+            super (url, container)
+            this.getJson ()
+                .then (data => this.handleData(data.contents)) 
+        }
+    
+        addProduct (element) {
+            this.getJson (API + '/addToBasket.json')
+                .then (response => {
+                    if (response.result) {
+                        let prodId = +element.dataset['id']
+                        let find = this.allProducts.find (item => item.id_product === prodId)
+                        if (find) {
+                            find.quantity ++
+                            this._updateCart (find)
+                        } else {
+                            let product = {
+                                id_product: prodId,
+                                price: +element.dataset['price'],
+                                product_name: element.dataset['name'],
+                                img: +element.dataset['image'],
+                                quantity: 1
+                            }
+                            this.allProducts.push(product)
+                            this.render ()
+                        }
+                    }
+                })
+        }
+    
+        // removeProduct (element) {
+        //     this.getJson (API + '/addToBasket.json')
+        //         .then (response => {
+        //             if (response.result) {
+        //                 let prodId = +element.dataset['id']
+        //                 let find = this.allProducts.find (item => item.id_product === prodId)
+    
+        //                 if (find) {
+        //                     find.quantity ++
+        //                     this._updateCart (find)
+        //                 } else {
+        //                     let product = {
+        //                         id_product: prodId,
+        //                         price: +element.dataset['price'],
+        //                         product_name: element.dataset['name'],
+        //                         img: +element.dataset['image'],
+        //                         quantity: 1
+        //                     }
+        //                     this.allProducts.push(product)
+        //                     this.render ()
+        //                 }
+        //             }
+        //         })
+        // }
+        _updateCart (product) {
+            console.log (product)
+            let block = document.querySelector (`.cart-item[data-id = "${product.id_product}"]`)
+            block.querySelector('.product-quantity').textContent = `Quantity: ${product.quantity}`
+            block.querySelector('.product-price').textContent = `${product.quantity * product.price}`
+        }
+        _init () {
+            document.querySelector ('.btn-cart').addEventListener ('click', () => {
+                //document.querySelector (this.container).classList.toggle ('invisible')
+                document.querySelector('.cart-block').classList.toggle('invisible')
+            })
+    
+            document.querySelector (this.container).addEventListener ('click', event => {
+                if (event.target.classList.contains('del-btn')) {
+                    //this.removeProduct (event.target)
+                    console.log (`Товар ${event.target.dataset.name} удален`)
+                }
+            })
+        }
     }
-}
+    
+
+//______________________________________________
+
+// // моя корзина
+// class Cart {
+//     constructor() {
+//         this.products = []
+//     }
+//     getSum() {
+//         let sum = 0
+//         this.products.forEach(el => {
+//             sum += el.price * el.count
+//         })
+//         return sum
+//     }
+//     getCount() {
+//         let count = 0
+//         this.products.forEach(el => {
+//             count += el.count
+//         })
+//         return count
+//     }
+//     getProduct(id) {
+//         return this.products.find(item => item.id == id)
+//     }    
+//     addProduct(id) {
+//         const catalogProduct = catalog.getProduct(id)
+//         const cartProduct = this.getProduct(id)
+//         if (catalogProduct == cartProduct) cartProduct.count++
+//         else {
+//             catalogProduct.count = 1
+//             this.products.push(catalogProduct)
+//         }
+//         this.show()
+//     }
+//     removeProduct(id) {
+//         let product = this.getProduct(id)
+//         let index = this.products.findIndex(item => item.id == id);
+//         if (product.count > 1) product.count--
+//         else this.products.splice(index, 1)
+//         if (this.products.length == 0) this.clear()
+//     }
+//     clear() {
+//         this.products = []
+//         cartBlock.innerHTML = 'Корзина пуста'
+//         cartCount.innerHTML = 'Корзина пуста'
+//     }
+//     show() {
+//         cartCount.innerHTML = `${this.getCount()} шт. на сумму ${this.getSum()}$`
+//         cartCount.classList.remove('hidden')
+//         setTimeout(() => cartCount.classList.add('hidden'), 2000)
+//     }
+
+//     render() {
+//         let htmlString = ``
+//         this.products.forEach(function (el) {
+//             htmlString += `
+//         <p>${el.name}</p>
+//         <p>${el.price * el.count}$</p>
+//         <p>
+//             <button class="plus-btn" data-id="${el.id}">+</button>
+//             <span>${el.count}</span>
+//             <button class="minus-btn" data-id="${el.id}">-</button>
+//         </p>
+//         `
+//         })
+//         htmlString += `<strong>ИТОГО:</strong><strong>${this.getSum()}$</strong><p></p>`
+//         htmlString += `<p></p><p></p><a href="#" class = "cart-clear">Очистить</a>`
+//         cartBlock.innerHTML = htmlString
+//     }
+// }
 //_____________________________________________________________________________________________________
 
-// создаем корзину, новый каталог, заполняем товарами и выводим
-let cart = new Cart()
-let catalog = new Catalog('https://raw.githubusercontent.com/berryllium/-js-gb-second-05.10/gorkun/students/Gorkun%20Dmitriy/project/db/catalog.json')
+let lists = {
+    Catalog: Item,
+    Cart: CartItem
+}
 
-//__________________________________________________________________
+let cart = new Cart ()
+let catalog = new Catalog (cart)
 
 
+// //События 
+// //обработчик для добавления товара в корзину
+// document.addEventListener('click', function (e) {
+//     if (e.target.classList.contains('buy-btn') || e.target.classList.contains('plus-btn')) {
+//         cart.addProduct(+e.target.dataset['id'])
+//         cart.render()
+//     }
+// })
 
+// // обработчик для удаления товара из корзины
+// cartBlock.addEventListener('click', function (e) {
+//     if (e.target.classList.contains('minus-btn')) {
+//         cart.removeProduct(+e.target.dataset['id'])
+//         if(cart.products.length) cart.render()
+//         else cart.clear()
+//     }
+// })
 
-//События 
-//обработчик для добавления товара в корзину
-document.addEventListener('click', function (e) {
-    if (e.target.classList.contains('buy-btn') || e.target.classList.contains('plus-btn')) {
-        cart.addProduct(+e.target.dataset['id'])
-        cart.render()
-    }
-})
+// // обработчик для очистки корзины
+// cartBlock.addEventListener('click', function (e) {
+//     if (e.target.classList.contains('cart-clear')) {
+//         cart.clear()
+//     }
+// })
 
-// обработчик для удаления товара из корзины
-cartBlock.addEventListener('click', function (e) {
-    if (e.target.classList.contains('minus-btn')) {
-        cart.removeProduct(+e.target.dataset['id'])
-        if(cart.products.length) cart.render()
-        else cart.clear()
-    }
-})
-
-// обработчик для очистки корзины
-cartBlock.addEventListener('click', function (e) {
-    if (e.target.classList.contains('cart-clear')) {
-        cart.clear()
-    }
-})
-
-// обработчик для показа содержимого корзины
-cartButton.addEventListener('click', function (e) {
-    cartBlock.classList.toggle('invisible')
-    // setTimeout(function () { cartBlock.classList.add('invisible') }, 15000)
-})
+// // обработчик для показа содержимого корзины
+// cartButton.addEventListener('click', function (e) {
+//     cartBlock.classList.toggle('invisible')
+//     // setTimeout(function () { cartBlock.classList.add('invisible') }, 15000)
+// })
