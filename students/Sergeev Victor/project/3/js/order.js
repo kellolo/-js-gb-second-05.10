d180917a88a6fd962a18b4057eb5c9f1b197f5b8
Victor <38391733+Essvitex@users.noreply.github.com>
2019-10-20 20:39:44 +0500

    доделано 4 ДЗ.

--- students/Sergeev Victor/project/3/js/order.js
+++ students/Sergeev Victor/project/3/js/order.js
@@ -1,31 +1,57 @@
 class Order{
-    static regexName = new RegExp(/[a-zA-Zа-яА-ЯёЁ']{2,}/, 'gi');
-    static regexPhone = new RegExp(/\+\d{1}\(\d{3}\)\d{3}-\d{4}/, 'gi');
-    static regexEmail = new RegExp(/^([a-z0-9_\.-]+)@([a-z0-9_\.-]+)\.([a-z\.]{2,6})$/, 'gi');
+    regexName = new RegExp(/[a-zA-Zа-яА-ЯёЁ']{2,}/, 'gi');
+    regexSurname = new RegExp(/[a-zA-Zа-яА-ЯёЁ']{2,}/, 'gi');
+    regexPhone = new RegExp(/\+\d{1}\(\d{3}\)\d{3}-\d{4}/, 'gi');
+    regexEmail = new RegExp(/^([a-z0-9_\.-]+)@([a-z0-9_\.-]+)\.([a-z\.]{2,6})$/, 'gi');
 
-    constructor() {
-        //this.name = name;
-        //this.phone = phone;
-        //this.email = email;
-        //this.msg = msg;
-        //alert(Order.regexName.test("Vs"));
-        //console.log(Order.regexPhone.test("+7(000)000-0000"));
-        alert(Order.regexEmail.test("vi.sergeev@yandex.ru"));
-        alert(Order.regexEmail.test("dsfdsf"));
-        alert(Order.regexEmail.test("mymail@mail.ru"));
-        alert(Order.regexEmail.test("my-mail@mail.ru"));
+    constructor(surname, name, email, phone) {
+        this.surname = surname;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.log = "ERROR:";
+
+        this.checkName(this.name);
+        this.checkSurname(this.surname);
+        this.checkEmail(this.email);
+        this.checkPhoneNumber(this.phone);
+
+        if(this.log.length > 7){
+            console.log(this.log);
+            alert(this.log);
+        }
+        else {
+            localStorage.setItem("totalSum", 0);
+            alert("Спасибо, что выбрали нас! Ваш заказ принят! Ждите звонка, скоро свяжется наш оператор.");
+        }
     }
 
     checkName(target){
+        let isValid = this.regexName.test(target);
+        if(!isValid){
+            this.log+=" * проверьте, что в имени нет цифр и больше 2 букв\n"
+        }
+    }
 
+    checkSurname(target){
+        let isValid = this.regexSurname.test(target);
+        if(!isValid){
+            this.log+=" * проверьте, что в фамилии нет цифр и больше 2 букв\n"
+        }
     }
 
     checkEmail(target){
-
+        let isValid = this.regexEmail.test(target);
+        if(!isValid){
+            this.log+=" * почтовый адрес не является валидным\n"
+        }
     }
 
     checkPhoneNumber(target){
-
+        let isValid = this.regexPhone.test(target);
+        if(!isValid){
+            this.log+=" * номер телефона не поддерживает формат +7(000)000-0000\n";
+        }
     }
 }
 
@@ -39,13 +65,14 @@ window.addEventListener("load", function(event) {
 
 let orderbtn = document.getElementsByClassName("btn-submit")[0];
 orderbtn.addEventListener('click', function (event) {
-    /*let inputsOfOrder = document.querySelectorAll(".flex-order-form input");
-    inputsOfOrder.forEach(inputValue=>{
-        alert(inputValue.value);
-    })
-    */
-    let order = new Order()
-    localStorage.setItem("totalSum", 0);
+    let inputsOfOrder = document.querySelectorAll(".flex-order-form input");
+    if(localStorage['totalSum']!= 0 && localStorage['totalSum'] != undefined){
+        let order = new Order(inputsOfOrder[0].value, inputsOfOrder[1].value, 
+            inputsOfOrder[2].value, inputsOfOrder[3].value);
+    }
+    else{
+        alert("Корзина товаров пуста!");
+    }
 });
     
 
