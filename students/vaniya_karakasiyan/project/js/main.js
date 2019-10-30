2be5c15b1d9baa6d2ad21cc5efebd4b37d258e78
vaniya-k <49368613+vaniya-k@users.noreply.github.com>
2019-10-30 14:43:24 +0300

    Add files via upload

--- students/vaniya_karakasiyan/project/js/main.js
+++ students/vaniya_karakasiyan/project/js/main.js
@@ -1,19 +1,7 @@
 const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses'
-const CART_URL = '/getBasket.json'
 
 let app = new Vue ({
     el: '#app',
-
-    data: {
-        catalogUrl: '/catalogData.json',
-        addToCartUrl: '/addToBasket.json',
-        removeFromCartUrl: '/deleteFromBasket.json',
-        imgCatalog: 'https://placehold.it/200x150',
-        imgCart: 'https://placehold.it/100x80',
-        products: [],
-        cart: [],
-        cartDisplay: false
-    },
 
     methods: {
         getJson (url) {
@@ -22,53 +10,12 @@ let app = new Vue ({
             .catch (err => {
                 console.log (err)
             })
-        },
-
-        addToCart (product) {
-            this.getJson (this.addToCartUrl)
-                .then (response => {
-                    if (response.result) {
-                        let prodId = +product.id_product
-                        let find = this.cart.find (cartItem => cartItem.id_product === prodId)
-                        if (find) {
-                            find.quantity ++
-                        } else {
-                            let cartItem = {
-                                id_product: prodId,
-                                price: product.price,
-                                product_name: product.product_name,
-                                img: this.imgCart,
-                                quantity: 1
-                            }
-                            this.cart.push(cartItem)
-                        }
-                    }
-                })
-        },
-
-        removeFromCart (product) {
-            this.getJson (this.removeFromCartUrl)
-                .then (response => {
-                    if (response.result) {
-                        let prodId = +product.id_product
-                        let find = this.cart.find (cartItem => cartItem.id_product === prodId)
-                        if (find.quantity > 1) {
-                            find.quantity --
-                        } else {
-                            this.cart.splice(this.cart.indexOf(find), 1)
-                        }
-                    }
-                })
         }
-
     },
 
-    mounted () {
-        this.getJson (this.catalogUrl)
-            .then (data => this.products = data)
+    components: {
+        'cart': cart,
+        'sort': sort,
+        'catalog': catalog
     }
 })
-
-//оставшиеся задания из третьего урока:
-//1. Переделайте makeGETRequest() так, чтобы она использовала промисы.
-//3* Переделайте GoodsList так, чтобы fetchGoods() возвращал промис, а render() вызывался в обработчике этого промиса.
