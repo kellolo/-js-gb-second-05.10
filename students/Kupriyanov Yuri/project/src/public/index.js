71170d980ad47aee3ed4c8a4784676c34b298dc3
mexicanstrat <Lokonikee@gmail.com>
2019-11-02 15:27:31 +0400

    8 lesson

--- students/Kupriyanov Yuri/project/src/public/index.js
+++
@@ -1,5 +0,0 @@
-import appMain from './js/main'
-import './style/normalize.css'
-import './style/style.css'
-
-const app = new Vue (appMain)
--- students/Кривец Роман/project/src/public/index.js
+++
@@ -1,7 +0,0 @@
-import '@babel/polyfill'
-import 'whatwg-fetch'
-import {app} from './js/main'
-import './css/normalize.css'
-import './css/style.css'
-
-const mainApp = new Vue(app);
